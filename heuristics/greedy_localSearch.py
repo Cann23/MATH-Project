7932373203fbe215fa854aa9626156cb00bf1246
can-beydogan <cannmettu@gmail.com>
2024-12-04 01:44:06 +0100

    fix local search

--- heuristics/greedy_localSearch.py
+++ heuristics/greedy_localSearch.py
@@ -1,33 +1,32 @@
 import numpy as np
 import time
 
-from common import read_data, calculate_objective
+from common import read_data, calculate_objective, is_feasible
 
-def local_search(D, n, N, d, m, selected):
-    best_solution = selected
+def local_search(D, n, N, d, m, solution):
+    # Initialize the best solution and objective
+    best_solution = solution
     best_objective = calculate_objective(D, n, N, d, m, best_solution)
-    improved = True
+    is_improved = True
 
     # Local search loop
-    while improved:
-        improved = False
-        for i in range(len(selected)):
-            for j in range(i + 1, len(selected)):
-                # Swap members i and j to create a neighbor solution
+    while is_improved:
+        is_improved = False
+        for i in range(len(solution)):
+            for j in range(i + 1, len(solution)):
+                # for finding neighbor solution swap members i and j in the solution 
                 new_solution = best_solution[:]
                 new_solution[i], new_solution[j] = new_solution[j], new_solution[i]
 
-                # Check the new objective
-                new_objective = calculate_objective(D, n, N, d, m, new_solution)
+                # check the new solution is feasible
+                if is_feasible(D, n, N, d, m, new_solution):
+                    # Check the new objective
+                    new_objective = calculate_objective(D, n, N, d, m, new_solution)
 
-                if new_objective > best_objective:
-                    best_solution = new_solution
-                    best_objective = new_objective
-                    improved = True
-                    break
-
-        # Stop the search if no improvement
-        if not improved:
-            break
-
+                    if new_objective > best_objective:
+                        best_solution = new_solution
+                        best_objective = new_objective
+                        is_improved = True
+                        break
+                
     return best_solution, best_objective
